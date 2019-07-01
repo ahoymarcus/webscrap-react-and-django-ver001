abb85974d0416a7af7aa746f77bdfb097892013f
marcus <marcusviniciusdasilveiraricha@gmail.com>
2019-07-01 17:12:44 -0300

    Refatorando a class views de scrapapp: funcoes get_target_page e get_met

--- myDjango/django_scrapy/webscrap/scrapapp/views.py
+++ myDjango/django_scrapy/webscrap/scrapapp/views.py
@@ -9,63 +9,79 @@ from bs4 import BeautifulSoup
 
 
 # Create your views here.
+
+scrapy_page = 'https://nerdstore.com.br/categoria/especiais/game-of-thrones/'
+
+content = bytearray()
+
+
 def index(request):
   
   print("You're looking at the index page of scrapapp application!")
   
-  r = requests.get('https://nerdstore.com.br/categoria/especiais/game-of-thrones/')
-  print('Requisição bem sucedida!', r)
-  content = r.content
+  get_target_page(scrapy_page)
   
   
   
   if request.method == 'GET':
-    path = request.path
-    #path = request.get_full_path()
-    print(path)
-    
-    produto = request.GET['produto']
-    print('Incoming request...')
-   
-    soup = BeautifulSoup(content, 'html.parser')
-    
-    # Scrapping
-    prod_title = soup.find(name='h2', attrs={'class' : 'woocommerce-loop-product__title'})
-    prod_link = soup.find(name='img', attrs={'class' : 'attachment-woocommerce_thumbnail size-woocommerce_thumbnail'})
-    prod_preco = soup.find(name='span', attrs={'class' : 'woocommerce-Price-amount'})
-   
-    nome_str = str(prod_title)
-    link_str = str(prod_link)
-    preco_str = str(prod_preco)
-    
-    itens = []
-    i = 0
-    while i < len(nome_str):
-      item = {
-        "produto": nome_str[i],
-        "link": link_str[i],
-        "preco": preco_str[i]
-      }
-      itens.append(item)
-      i += 1
-    
-    
-    if produto is not None and produto != '':
-      print("Let's get started scrapping: ", produto)
-    
-      # Buscando a posição da estrutura dentro da lista retornada
-      prod_indice = itens.index(produto)
-      
-      selecao = {
-        "produto": nome_str[prod_indice],
-        "link": link_str[prod_indice],
-        "preco": preco_str[prod_indice]
-      }
-      
-      return JsonResponse(selecao)
+    get_met(request)
     
      
   return JsonResponse(itens)
+
+
+def get_target_page(web_page):
+
+  r = requests.get(scrapy_page)
+  print('Requisição bem sucedida!', r)
+  content = r.content
   
-
-
+  print("Tipo do objeto:", type(content))
+  
+ 
+def get_met(request): 
+  path = request.path
+  #path = request.get_full_path()
+  print(path)
+  
+  produto = request.GET['produto']
+  print('Incoming request...')
+ 
+  soup = BeautifulSoup(content, 'html.parser')
+  
+  # Scrapping
+  prod_title = soup.find(name='h2', attrs={'class' : 'woocommerce-loop-product__title'})
+  prod_link = soup.find(name='img', attrs={'class' : 'attachment-woocommerce_thumbnail size-woocommerce_thumbnail'})
+  prod_preco = soup.find(name='span', attrs={'class' : 'woocommerce-Price-amount'})
+ 
+  nome_str = str(prod_title)
+  link_str = str(prod_link)
+  preco_str = str(prod_preco)
+  
+  itens = []
+  i = 0
+  while i < len(nome_str):
+    item = {
+      "produto": nome_str[i],
+      "link": link_str[i],
+      "preco": preco_str[i]
+    }
+    itens.append(item)
+    i += 1
+  
+  
+  if produto is not None and produto != '':
+    print("Let's get started scrapping: ", produto)
+  
+    # Buscando a posição da estrutura dentro da lista retornada
+    prod_indice = itens.index(produto)
+    
+    selecao = {
+      "produto": nome_str[prod_indice],
+      "link": link_str[prod_indice],
+      "preco": preco_str[prod_indice]
+    }
+    
+    return JsonResponse(selecao)
+  
+  
